f96973bba2b9c010e56fbc36e411d86df4c48ae6
LudiosCode <59086310+LudiosCode@users.noreply.github.com>
2021-05-17 00:38:44 +0200

    "movement"

--- Figur.py
+++ Figur.py
@@ -22,11 +22,18 @@ tWeiss = []
 tSchwarz = []
 
 
+squares = []
 
+#alliedPieces
+aPieceLocs = []
+
+#opposedPieces
+oPieceLocs = []
 
 class Piece:
 
     image = -1
+
 
     def __init__(self):
         self.row = 0
@@ -56,17 +63,20 @@ class King(Piece):
 
         squares = []
 
-        for x in range(-1, 1):
-            for y in range(-1, 1):
-                pass
-
-
-
         for p in pieces:
             if p.color == self.color:
-                if (p.column, p.row) in squares:
-                    squares.remove((p.column, p.row))
+                aPieceLocs.append((p.column, p.row))
 
+        for x in range(-1, 1):
+            for y in range(-1, 1):
+
+                s = (self.column + x, self.row + y)
+
+                if s not in aPieceLocs:
+                    squares.append(s)
+                elif s in oPieceLocs:
+                    if self.movelegal(s):
+                        squares.append(s)
 
 
         return squares
@@ -77,11 +87,89 @@ class Queen(Piece):
 class Bishop(Piece):
     image = 2
 
+    def ways(self, pieces):
+
+        squares = []
+
+        for p in pieces:
+            if p.color == self.color:
+                aPieceLocs.append((p.column, p.row))
+
+
+        for d in range(4):
+            for x in range(1, 8):
+
+                if d == 0:
+                    s = (self.column - x, self.row - x)
+                elif d == 1:
+                    s = (self.column + x, self.row - x)
+                elif d == 2:
+                    s = (self.column - x, self.row + x)
+                elif d == 3:
+                    s = (self.column + x, self.row + x)
+
+                if not (0 <= s[0] <= 7) or not( 0 <= s[1] <= 7):
+                    break
+                else:
+
+                    if s in aPieceLocs:
+                        break
+
+                    #squares.append(s)
+
+                    if s not in aPieceLocs:
+                        squares.append(s)
+                    elif s in oPieceLocs:
+                        if self.movelegal(s):
+                            squares.append(s)
+
+
+        return squares
+
 class Knight(Piece):
     image = 3
 
 class Rook(Piece):
     image = 4
+
+    def ways(self, pieces):
+
+        squares = []
+
+        for p in pieces:
+            if p.color == self.color:
+                aPieceLocs.append((p.column, p.row))
+
+
+        for d in range(4):
+            for x in range(1, 8):
+
+                if d == 0:
+                    s = (self.column - x, self.row)
+                elif d == 1:
+                    s = (self.column + x, self.row)
+                elif d == 2:
+                    s = (self.column, self.row - x)
+                elif d == 3:
+                    s = (self.column, self.row + x)
+
+                if not (0 <= s[0] <= 7) or not (0 <= s[1] <= 7):
+                    break
+                else:
+
+                    if s in aPieceLocs:
+                        break
+
+                    #squares.append(s)
+
+                    if s not in aPieceLocs:
+                        squares.append(s)
+                    elif s in oPieceLocs:
+                        if self.movelegal(s):
+                            squares.append(s)
+
+
+        return squares
 
 class Pawn(Piece):
     image = 5
